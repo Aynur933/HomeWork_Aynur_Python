1ec7a1962fefa111fb0e9ca6ca81be10c98eb189
Aynur933 <baigildin.aynur@yandex.ru>
2024-10-02 01:58:32 +0500

    Домашнее задание 1

---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,17 @@
+def today(num):
+    print(num, end="")
+
+
+today(8)
+today(8)
+today(0)
+today(0)
+today(5)
+today(5)
+today(5)
+today(3)
+today(5)
+today(3)
+today(5)
+
+print("  Проще позвонить,чем у кого то занимать!")
